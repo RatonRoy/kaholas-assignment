6ed0f269fd2adf1f2e525c8468a20845494f0425
RatonRoy <royraton18@gmail.com>
2022-06-25 18:06:21 +0600

    install the tailwind css and daisyUi components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-2xl text-red-500">
+          I am a heading text 
+      </h1>
+      <button class="btn btn-secondary">Button</button>
     </div>
   );
 }
